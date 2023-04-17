8568fb158796983e2f527f8693653b62fde42869
Мельник Денис <den-melnik-1996@mail.ru>
2023-04-17 22:54:47 +0300

    коментарий)

--- str_func.py
+++ str_func.py
@@ -1,3 +1,16 @@
 def str(str):
+   '''
+   комментарий.
+   :param str:
+   :return:
+   '''
    return str.upper()
+
+def new_str(str):
+   '''
+   выводит первые буквы заглавные.
+   :return:
+   '''
+   pass
+
     
